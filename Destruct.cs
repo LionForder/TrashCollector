78bafc944850a8e4aae5370fe0333fe416067ae6
LionForder <goomano12@gmail.com>
2022-05-14 10:53:59 +0500

    Garbage collector

---
+++ Destruct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace TrashCollector
+{
+    public class Destruct
+    {
+        protected internal bool deleted = false;
+        public Destruct()
+        {
+            Console.WriteLine("Object has been to created!");
+        }
+        ~Destruct()
+        {
+            Console.WriteLine("Object has been to deleted!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TrashCollector
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Destruct first = new Destruct();
+            Destruct second = new Destruct();
+            Destruct third = new Destruct();
+            // достаточно будет удалить ссылку
+            first = null;
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            Console.WriteLine(second.deleted);
+            Console.WriteLine(third.deleted);
+            second = third;
+            second.deleted = true; // ссылается на third => и его изменяет
+            Console.WriteLine(second.deleted);
+            Console.WriteLine(third.deleted);
+
+        }
+    }
+}
